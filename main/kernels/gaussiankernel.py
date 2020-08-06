3d6d7066781b35577e38a493c14ec2fd67c54985
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-06 02:50:50 +0100

    improved covariance derivation, improved get_gradients method

--- main/kernels/gaussiankernel.py
+++ main/kernels/gaussiankernel.py
@@ -8,16 +8,12 @@ class GaussianKernel(BaseKernel):
 
     def get_cov(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
         a2 = self.amplitude_squared
-        l_s = self.length_scale
-        return a2 * np.exp(-1 / (2 * l_s ** 2) * self._get_distance(X, X_) ** 2)
+        l_s2 = self.length_scale**2
+        d2 = self._get_distance(X, X_) ** 2
+        return a2 * np.exp(-0.5 * d2 / l_s2)
 
     def get_gradients(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
-        K = self.get_cov(X, X_)
-        d2 = self._get_distance(X, X_) ** 2
-        dK_damplitude = K / self.amplitude * 2
-        dK_dlength_scale = self.amplitude_squared * self.length_scale ** (-3) * d2 @ K
-
-        return np.asarray([dK_damplitude, dK_dlength_scale])
+        return np.asarray([self.get_dk_damplitude(X, X_), self.get_dK_dlength_scale(X, X_)])
 
     def get_dK_dlength_scale(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
         d2 = self._get_distance(X, X_) ** 2
---
+++ main/utils/plotter.py
@@ -0,0 +1,41 @@
+from main.processes.baseprocess import BaseProcess
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Plotter:
+    def __init__(self, process: BaseProcess):
+        self.process = process
+
+    def plot(self, n_samples: int = 0) -> None:
+        mean = self.process.get_predictions()[0]
+        if mean.shape == ():
+            xx = np.linspace(0, 50, 50)
+            xx = xx.reshape((-1,))
+            mean = self.process.get_predictions(xx)[0].flatten()
+            mean.resize((xx.shape[0],), refcheck=False)
+            std = self.process.get_std(xx).flatten()
+            for _ in range(n_samples):
+                plt.plot(xx, self.process.get_sample(xx))
+            plt.plot(xx, mean, c='b')
+            plt.plot(xx, mean + 3 * std, c='r')
+            plt.plot(xx, mean - 3 * std, c='b')
+            plt.fill_between(xx, mean - 3 * std, mean + 3 * std, alpha=0.2, color='m')
+            plt.show()
+        else:
+            xx = self.process._X
+            xx = xx.reshape((xx.shape[0],))
+            mean = mean.flatten()
+            std = self.process.get_std()
+            std = std.flatten()
+            for _ in range(n_samples):
+                plt.plot(xx, self.process.get_sample(xx.reshape((-1, 1))))
+            plt.plot(xx, mean, c='b')
+            plt.plot(xx, mean + 3 * std, c='r')
+            plt.plot(xx, mean - 3 * std, c='b')
+            plt.fill_between(xx, mean - 3 * std, mean + 3 * std, alpha=0.2, color='m')
+            plt.scatter(xx, self.process._y, c='g', marker='+')
+            plt.show()
+
+    def __call__(self, process: BaseProcess) -> None:
+        self.process = process
