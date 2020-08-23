16300544b2828144db2f78c93d6ece83f5b3a650
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-23 20:42:52 +0100

    minor style fixes and method improvements

--- main/processes/baseregressionprocess.py
+++ main/processes/baseregressionprocess.py
@@ -3,28 +3,25 @@ from main.kernels.basekernel import BaseKernel
 import abc
 
 
-class BaseProcess(metaclass=abc.ABCMeta):
+class BaseRegressionProcess(metaclass=abc.ABCMeta):
     def __init__(self, kernel: BaseKernel, noise_scale: float = 0.5, X: np.ndarray = None,
                  y: np.ndarray = None) -> None:
         self.kernel = kernel
         self.log_noise_scale = np.log(noise_scale)
-        if X and y:
+        if self.data_is_valid(X, y):
             self.initialise_data(X, y)
-        elif not (X and y):
-            self._X = np.asarray([])
-            self._y = np.asarray([])
+        elif X is None and y is None:
+            self.X = np.asarray([])
+            self.y = np.asarray([])
             self._cov = np.asarray([])
         else:
             raise ValueError('Improper data passed in. Make sure both X and y are valid')
 
     @staticmethod
-    def _data_is_valid(X: np.ndarray, y: np.ndarray):
-        if 1 not in y.shape:
-            return False
-            # raise ValueError('y must be one dimensional')
-        if X.shape[0] != y.shape[0]:
-            return False
-        return True
+    def data_is_valid(X: np.ndarray, y: np.ndarray) -> bool:
+        if X is not None and y is not None:
+            return X.shape[0] == y.shape[0]
+        return False
 
     def initialise_data(self, X: np.ndarray, y: np.ndarray) -> None:
         X = np.asarray(X)
@@ -32,12 +29,12 @@ class BaseProcess(metaclass=abc.ABCMeta):
         if not self._data_is_valid(X, y):
             raise ValueError('The passed in data is of invalid format, {X}\n{y}'.format(X=X, y=y))
 
-        self._X = X
-        self._y = y
+        self.X = X
+        self.y = y
         self.set_covariance()
 
     def set_covariance(self):
-        self._cov = self.kernel(self._X, self._X)
+        self._cov = self.kernel(self.X, self.X)
 
     def set_kernel_parameters(self, amplitude: float, length_scale: float, as_log=True) -> None:
         if as_log:
@@ -83,7 +80,7 @@ class BaseProcess(metaclass=abc.ABCMeta):
         pass
 
     def get_dK_y_dnoise(self) -> np.ndarray:
-        return 2 * self.noise_scale * np.identity(self._X.shape[0])
+        return 2 * self.noise_scale * np.identity(self.X.shape[0])
 
     @property
     def noise_scale(self) -> float:
