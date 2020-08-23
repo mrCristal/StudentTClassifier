5ef938acdac847e38a53c6c4e42f7ce6938583ca
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-23 20:31:14 +0100

    added doc strings, optimised get_gradients function

--- main/kernels/matern52kernel.py
+++ main/kernels/matern52kernel.py
@@ -4,10 +4,19 @@ from numpy import sqrt as sr
 
 
 class Matern52Kernel(BaseKernel):
+    """
+    Classs implementing the Matern 5/2 kernel function
+    """
     def __init__(self, amplitude: float, length_scale: float) -> None:
         super().__init__(amplitude=amplitude, length_scale=length_scale)
 
     def get_cov(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
+        """
+        Returns the covariance as given by the Gaussian (RBF) algorithm
+        :param X: n x p np.ndarray, data points where the covariance should be evaluated
+        :param X_: n x p np.ndarray, data points where the covariance should be evaluated
+        :return: n x n np.ndarray, covariance matrix
+        """
         a2 = self.amplitude_squared
         l_s = self.length_scale
         d = self._get_distance(X, X_)
@@ -16,16 +25,21 @@ class Matern52Kernel(BaseKernel):
         return a2 * (1 + m + m2) * np.exp(-m)
 
     def get_gradients(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
-        K = self.get_cov(X, X_)
-        d = self._get_distance(X, X_)
-        dK_damplitude = K / self.amplitude * 2
-        l_s = self.length_scale
-        m = sr(5) * d / l_s
-        dK_dlength_scale = d ** 2 * self.amplitude_squared * (5 * l_s + 5 ** 1.5 * d) * np.exp(-m) / (3 * l_s ** 4)
-
-        return np.asarray([dK_damplitude, dK_dlength_scale])
+        """
+        Returns the gradients of the covariance function w.r.t parameters at for the given data points
+        :param X: n x p np.ndarray, data points where the gradient should be evaluated
+        :param X_: n x p np.ndarray, data points where the gradient should be evaluated
+        :return: n x n np.ndarray, evaluated gradient
+        """
+        return np.asarray([self.get_dk_damplitude(X, X_), self.get_dK_dlength_scale(X, X_)])
 
     def get_dK_dlength_scale(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
+        """
+        Returns the gradient of the covariance function w.r.t. the length scale for the give data points
+        :param X: n x p np.ndarray, data points where the gradient should be evaluated
+        :param X_: n x p np.ndarray, data points where the gradient should be evaluated
+        :return: n x n np.ndarray, evaluated gradient
+        """
         d = self._get_distance(X, X_)
         l_s = self.length_scale
         m = sr(5) * d / l_s
