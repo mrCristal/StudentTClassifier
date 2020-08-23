3c27a8892b676dbf91973895bf2a5da5658be0c9
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-23 20:47:17 +0100

    addded doc strings, minor bug fixes

--- main/utils/plotter.py
+++ main/utils/plotter.py
@@ -1,17 +1,29 @@
-from main.processes.baseprocess import BaseProcess
+from main.processes.baseregressionprocess import BaseRegressionProcess
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 class Plotter:
-    def __init__(self, process: BaseProcess):
+    """
+    Simple plotter for the BaseRegressionProcess
+    """
+    def __init__(self, process: BaseRegressionProcess):
         self.process = process
 
     def plot(self, n_samples: int = 0, with_y=False) -> None:
+        """
+        Plots samples for predictions over current points
+        If there are no current training points, the plotted samples will be of the prior from -5 to 5
+        If there are training points in 1 dimension then n samples are plotted with the x axis as the training points
+        If there are training points in more than 1 dimension then n samples are plotted with the x axis as the
+            index of the training points
+        :param n_samples: int, the number of points to plot
+        :param with_y: bool, specifies if tragets y are to be a plotted as scatter graph on top
+        :return: None
+        """
         mean = self.process.get_predictions()[0]
         if mean.shape == ():
-            xx = np.linspace(0, 50, self.process._X.shape[0])
-            xx = xx.reshape((-1,))
+            xx = np.linspace(-5, 5, 50)
             mean = self.process.get_predictions(xx)[0].flatten()
             mean.resize((xx.shape[0],), refcheck=False)
             std = self.process.get_std(xx).flatten()
@@ -22,24 +34,25 @@ class Plotter:
             plt.plot(xx, mean - 3 * std, c='b')
             plt.fill_between(xx, mean - 3 * std, mean + 3 * std, alpha=0.2, color='m')
             plt.show()
-        if self.process._X.shape[1] >= 2:
-            xx = np.linspace(0, self.process._X.shape[0], self.process._X.shape[0])
+
+        elif self.process.X.shape[1] >= 2:
+            xx = np.linspace(0, self.process.X.shape[0], self.process.X.shape[0])
             xx = xx.reshape((-1,))
             mean = mean.flatten()
             std = self.process.get_std()
             std = std.flatten()
             for _ in range(n_samples):
-                plt.plot(self.process.get_sample(self.process._X))
+                plt.plot(self.process.get_sample(self.process.X))
             plt.plot(xx, mean, c='b')
             plt.plot(xx, mean + 3 * std, c='r')
             plt.plot(xx, mean - 3 * std, c='b')
             plt.fill_between(xx, mean - 3 * std, mean + 3 * std, alpha=0.2, color='m')
             if with_y:
-                plt.scatter(xx, self.process._y, c='g', marker='+')
+                plt.scatter(xx, self.process.y, c='g', marker='+')
             plt.show()
 
         else:
-            xx = self.process._X
+            xx = self.process.X
             xx = xx.reshape((xx.shape[0],))
             mean = mean.flatten()
             std = self.process.get_std()
@@ -51,8 +64,13 @@ class Plotter:
             plt.plot(xx, mean - 3 * std, c='b')
             plt.fill_between(xx, mean - 3 * std, mean + 3 * std, alpha=0.2, color='m')
             if with_y:
-                plt.scatter(xx, self.process._y, c='g', marker='+')
+                plt.scatter(xx, self.process.y, c='g', marker='+')
             plt.show()
 
-    def __call__(self, process: BaseProcess) -> None:
+    def __call__(self, process: BaseRegressionProcess) -> None:
+        """
+        Updates the process attribute
+        :param process: BaseRegressionProcess, the new regression process instance to use
+        :return: None
+        """
         self.process = process
