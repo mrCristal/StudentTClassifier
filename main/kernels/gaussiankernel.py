c680c549f49e75d49d62fc42293ba771585f3159
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-23 20:37:45 +0100

    removed redundant get_gradients method

--- main/kernels/gaussiankernel.py
+++ main/kernels/gaussiankernel.py
@@ -21,15 +21,6 @@ class GaussianKernel(BaseKernel):
         d2 = self._get_distance(X, X_) ** 2
         return a2 * np.exp(-0.5 * d2 / l_s2)
 
-    def get_gradients(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
-        """
-        Returns the gradients of the covariance function w.r.t parameters at for the given data points
-        :param X: n x p np.ndarray, data points where the gradient should be evaluated
-        :param X_: n x p np.ndarray, data points where the gradient should be evaluated
-        :return: n x n np.ndarray, evaluated gradient
-        """
-        return np.asarray([self.get_dk_damplitude(X, X_), self.get_dK_dlength_scale(X, X_)])
-
     def get_dK_dlength_scale(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
         """
         Returns the gradient of the covariance function w.r.t. the length scale for the give data points
