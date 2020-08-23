139c9b11897544f84afe0e86a981fdfe8d41842d
Bogdan Cristal <bc1819@ic.ac.uk>
2020-08-23 20:36:12 +0100

    added doc strings, removed unnecessary get_gradients function, fixed bugin length scale grad method

--- main/kernels/oukernel.py
+++ main/kernels/oukernel.py
@@ -3,26 +3,34 @@ import numpy as np
 
 
 class OUKernel(BaseKernel):
+    """
+    Class implementing the Ornstein - Uhlenbeck kernel function
+    """
     def __init__(self, amplitude: float, length_scale: float) -> None:
         super().__init__(amplitude=amplitude, length_scale=length_scale)
 
     def get_cov(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
+        """
+        Returns the covariance as given by the OU algorithm
+        :param X: n x p np.ndarray, data points where the covariance should be evaluated
+        :param X_: n x p np.ndarray, data points where the covariance should be evaluated
+        :return: n x n np.ndarray, covariance matrix
+        """
         a2 = self.amplitude_squared
         l_s = self.length_scale
         return a2 * np.exp(-1 / l_s * self._get_distance(X, X_))
 
     def get_dK_dlength_scale(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
+        """
+        Returns the gradient of the covariance function w.r.t. the length scale for the give data points
+        :param X: n x p np.ndarray, data points where the gradient should be evaluated
+        :param X_: n x p np.ndarray, data points where the gradient should be evaluated
+        :return: n x n np.ndarray, evaluated gradient
+        """
         d = self._get_distance(X, X_)
         K = self.get_cov(X, X_)
-        dK_dlength_scale = d / (self.length_scale ** 2) @ K
+        dK_dlength_scale = d / (self.length_scale ** 2) * K
         return dK_dlength_scale
-
-    def get_gradients(self, X: np.ndarray, X_: np.ndarray) -> np.ndarray:
-        K = self.get_cov(X, X_)
-        d = self._get_distance(X, X_)
-        dK_damplitude = K / self.amplitude * 2
-        dK_dlength_scale = d / (self.length_scale ** 2) @ K
-        return np.asarray([dK_damplitude, dK_dlength_scale])
 
     @property
     def type(self) -> str:
